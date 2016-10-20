37b010c57523cb31297ba0425e44dab6d5623d57
André Pena <andrerpena@gmail.com>
2016-10-20 20:34:09 -0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pena from './Pena';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pena/>
+      </div>
+    );
+  }
+}
+
+export default App;
